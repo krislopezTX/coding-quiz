c5e45236af2d38e878f922b0a794603024eaafc0
Kris Lopez <krislopeztx@yahoo.com>
2021-12-12 23:43:22 -0600

    added js

---
+++ assets/js/questions.js
@@ -0,0 +1,12 @@
+var questions = [
+    {
+        title: "Booleans express to:",
+        choices: ['true/false', 'yes/no', 'confirm/deny', 'aye/nay'],
+        answer: 'true/false',
+    },
+    {
+        title: "Which one of the following are valid string methods?",
+        choices: ['.length', '.includes()', '.is', '.map()'],
+        answer: '.includes()',
+    },
+];
