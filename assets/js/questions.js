bf8edd82b5f5cecffd8ce2b17d668bfae5c1486d
Kris Lopez <krislopeztx@yahoo.com>
2021-12-13 00:12:40 -0600

    updated code

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,12 +1,28 @@
+// The array of questions for quiz
 var questions = [
     {
-        title: "Booleans express to:",
-        choices: ['true/false', 'yes/no', 'confirm/deny', 'aye/nay'],
-        answer: 'true/false',
+        title: "Commonly Used data types DO NOT include:",
+        choices: ["stings", "alerts", "booleans", "numbers"],
+        answer: "alerts"
     },
     {
-        title: "Which one of the following are valid string methods?",
-        choices: ['.length', '.includes()', '.is', '.map()'],
-        answer: '.includes()',
+        title: "The condition in an if / else statment is enclosed within _____.",
+        choices: ["parentheses", "quotes", "curly brackets", "square brackets"],
+        answer: "parentheses"
     },
-];
+    {
+        title: "What javascipt method can we use to select an html element?",
+        choices: ["document.queryselector()", "document.getElementChild", "document.getElementById", "Both 1 and 3"],
+        answer: "Both 1 and 3"
+    },
+    {
+        title: "What html tag is NOT included in the HEAD tag?",
+        choices: ["link", "meta", "title", "header"],
+        answer: "header"
+    },
+    {
+        title: "What attribute is used in html to decorate content?",
+        choices: ["css", "class", "src", "style"],
+        answer: "style"
+    }
+]
